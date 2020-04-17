d9547876d244c23fae56c9577d600dcfab66d67c
Mike Jarmy <mjarmy@gmail.com>
2020-04-17 09:30:36 -0400

    begin working on new seal migration test

--- vault/testing.go
+++ vault/testing.go
@@ -1430,6 +1430,7 @@ func NewTestCluster(t testing.T, base *CoreConfig, opts *TestClusterOptions) *Te
 		coreConfig.CacheSize = base.CacheSize
 		coreConfig.PluginDirectory = base.PluginDirectory
 		coreConfig.Seal = base.Seal
+		coreConfig.UnwrapSeal = base.UnwrapSeal
 		coreConfig.DevToken = base.DevToken
 		coreConfig.EnableRaw = base.EnableRaw
 		coreConfig.DisableSealWrap = base.DisableSealWrap
